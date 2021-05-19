3917a6e362117ab0f42c0bce895e22aa14d1f1c5
BugGlitchy64 <crtcomputer@tutanota.com>
2021-05-19 21:37:00 +0800

    update video

--- source/VideoState.hx
+++ source/VideoState.hx
@@ -16,84 +16,37 @@ import openfl.Lib;
 
 using StringTools;
 
-class VideoState extends MusicBeatState
+class VideoState extends FlxSprite
 {
 	public var leSource:String = "";
 	//public var transClass:FlxState;
 	public var transFunction:Void->Void;
-	public var txt:FlxText;
-	public var fuckingVolume:Float = 1;
 	public var notDone:Bool = true;
-	public var vidSound:FlxSound;
-	public var useSound:Bool = false;
-	public var soundMultiplier:Float = 1;
 	public var prevSoundMultiplier:Float = 1;
 	public var videoFrames:Int = 0;
 	public var defaultText:String = "";
 	public var doShit:Bool = false;
 
-	public function new(source:String)
+	public function new(source:String, frames:Int)
 	{
 		super();
-		
-		leSource = source;
 		//transClass = toTrans;
-	}
-	
-	override function create()
-	{
-		super.create();
 		FlxG.autoPause = false;
 		doShit = false;
 		
-		if (GlobalVideo.isWebm)
-		{
-		videoFrames = Std.parseInt(Assets.getText(leSource.replace(".webm", ".txt")));
-		}
+		videoFrames = frames;
 		
-		fuckingVolume = FlxG.sound.music.volume;
-		FlxG.sound.music.volume = 0;
 		var isHTML:Bool = false;
 		#if web
 		isHTML = true;
 		#end
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
-		add(bg);
-		var html5Text:String = "You Are Not Using HTML5...\nThe Video Didnt Load!";
-		if (isHTML)
-		{
-			html5Text = "You Are Using HTML5!";
-		}
-		defaultText = "If Your On HTML5\nTap Anything...\nThe Bottom Text Indicates If You\nAre Using HTML5...\n\n" + html5Text;
-		txt = new FlxText(0, 0, FlxG.width,
-			defaultText,
-			32);
-		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
-		txt.screenCenter();
-		add(txt);
 
-		if (GlobalVideo.isWebm)
-		{
-			if (Assets.exists(leSource.replace(".webm", ".ogg"), MUSIC) || Assets.exists(leSource.replace(".webm", ".ogg"), SOUND))
-			{
-				useSound = true;
-				vidSound = FlxG.sound.play(leSource.replace(".webm", ".ogg"));
-			}
-		}
-
-		GlobalVideo.get().source(leSource);
+		GlobalVideo.get().source(source);
 		GlobalVideo.get().clearPause();
-		if (GlobalVideo.isWebm)
-		{
-			GlobalVideo.get().updatePlayer();
-		}
+		GlobalVideo.get().updatePlayer();
 		GlobalVideo.get().show();
-		if (GlobalVideo.isWebm)
-		{
-			GlobalVideo.get().restart();
-		} else {
-			GlobalVideo.get().play();
-		}
+		GlobalVideo.get().restart();
+		GlobalVideo.get().togglePause();
 		
 		/*if (useSound)
 		{*/
@@ -101,7 +54,6 @@ class VideoState extends MusicBeatState
 		
 			/*new FlxTimer().start(0.1, function(tmr:FlxTimer)
 			{*/
-				vidSound.time = vidSound.length * soundMultiplier;
 				/*new FlxTimer().start(1.2, function(tmr:FlxTimer)
 				{
 					if (useSound)
@@ -118,74 +70,19 @@ class VideoState extends MusicBeatState
 	{
 		super.update(elapsed);
 		
-		if (useSound)
-		{
-			var wasFuckingHit = GlobalVideo.get().webm.wasHitOnce;
-			soundMultiplier = GlobalVideo.get().webm.renderedCount / videoFrames;
-			
-			if (soundMultiplier > 1)
-			{
-				soundMultiplier = 1;
-			}
-			if (soundMultiplier < 0)
-			{
-				soundMultiplier = 0;
-			}
-			if (doShit)
-			{
-				var compareShit:Float = 50;
-				if (vidSound.time >= (vidSound.length * soundMultiplier) + compareShit || vidSound.time <= (vidSound.length * soundMultiplier) - compareShit)
-					vidSound.time = vidSound.length * soundMultiplier;
-			}
-			if (wasFuckingHit)
-			{
-			if (soundMultiplier == 0)
-			{
-				if (prevSoundMultiplier != 0)
-				{
-					vidSound.pause();
-					vidSound.time = 0;
-				}
-			} else {
-				if (prevSoundMultiplier == 0)
-				{
-					vidSound.resume();
-					vidSound.time = vidSound.length * soundMultiplier;
-				}
-			}
-			prevSoundMultiplier = soundMultiplier;
-			}
-		}
-		
-		if (notDone)
-		{
-			FlxG.sound.music.volume = 0;
-		}
 		GlobalVideo.get().update(elapsed);
 		
 		if (GlobalVideo.get().ended || GlobalVideo.get().stopped)
 		{
-			txt.visible = false;
-			GlobalVideo.get().hide();
 			GlobalVideo.get().stop();
 		}
 		
 		if (GlobalVideo.get().ended)
 		{
 			notDone = false;
-			FlxG.sound.music.volume = fuckingVolume;
-			FlxG.autoPause = true;
-			//FlxG.switchState(transClass);
-			transFunction();
-		}
-		
-		if (GlobalVideo.get().played || GlobalVideo.get().restarted)
-		{
-			GlobalVideo.get().show();
 		}
 		
 		GlobalVideo.get().restarted = false;
-		GlobalVideo.get().played = false;
 		GlobalVideo.get().stopped = false;
 		GlobalVideo.get().ended = false;
 	}
